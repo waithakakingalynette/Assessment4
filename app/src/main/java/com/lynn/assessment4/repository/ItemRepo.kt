7bf2cd8ed896a5817138742b74814611fb9194f4
waithakakingalynette <warukiralynette@gmail.com>
2023-08-24 12:43:04 +0300

    made changes on my app

--- app/src/main/java/com/lynn/assessment4/repository/ItemRepo.kt
+++ app/src/main/java/com/lynn/assessment4/repository/ItemRepo.kt
@@ -13,25 +13,3 @@ class ItemRepo {
         return apiClient.getItems()
     }
 }
-
-
-//package com.lynn.assessment4.repository
-//
-//import com.lynn.assessment4.api.ApiClient
-//import com.lynn.assessment4.api.ApiInterface
-//import com.lynn.assessment4.model.Items
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.withContext
-//import retrofit2.Response
-//
-//class ItemRepo {
-//    val apiClient= ApiClient.buildApiClient(ApiInterface::class.java)
-//    suspend fun getItems(): Response<List<Items>> {
-//        return withContext(Dispatchers.IO){
-//            apiClient.getItems()
-//        }
-//    }
-//}
-
-
-
